f289f5a5280c8f3784e0c871653eae5c3b6c7b09
sharkinsspatial <sharkinsgis@gmail.com>
2023-03-15 07:50:26 -0600

    Refactor use of load_into_pgstac and move collection load logic.

--- lib/ingestor-api/runtime/src/ingestor.py
+++ lib/ingestor-api/runtime/src/ingestor.py
@@ -3,16 +3,10 @@ from datetime import datetime
 from typing import TYPE_CHECKING, Iterator, List, Optional, Sequence
 
 from boto3.dynamodb.types import TypeDeserializer
-from pypgstac.db import PgstacDB
 
 from .dependencies import get_settings, get_table
 from .schemas import Ingestion, Status
-from .utils import (
-    IngestionType,
-    convert_decimals_to_float,
-    get_db_credentials,
-    load_into_pgstac,
-)
+from .utils import get_db_credentials, load_items
 
 if TYPE_CHECKING:
     from aws_lambda_typing import context as context_
@@ -64,24 +58,14 @@ def handler(event: "events.DynamoDBStreamEvent", context: "context_.Context"):
         print("No queued ingestions to process")
         return
 
-    items = [
-        # NOTE: Important to deserialize values to convert decimals to floats
-        convert_decimals_to_float(ingestion.item)
-        for ingestion in ingestions
-    ]
-
-    creds = get_db_credentials(os.environ["DB_SECRET_ARN"])
-
     # Insert into PgSTAC DB
     outcome = Status.succeeded
     message = None
     try:
-        with PgstacDB(dsn=creds.dsn_string, debug=True) as db:
-            load_into_pgstac(
-                db=db,
-                ingestions=items,
-                table=IngestionType.items,
-            )
+        load_items(
+            creds=get_db_credentials(os.environ["DB_SECRET_ARN"]),
+            ingestions=ingestions,
+        )
     except Exception as e:
         print(f"Encountered failure loading items into pgSTAC: {e}")
         outcome = Status.failed
