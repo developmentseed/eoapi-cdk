972fca282e4218406ce2ba51603dd18b55290b58
hrodmn <henry.rodman@gmail.com>
2025-02-03 13:01:14 -0600

    undo whitespace change

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -171,9 +171,7 @@ export class PgStacDatabase extends Construct {
       this._pgBouncerServer.node.addDependency(bootstrapper);
 
       this.pgstacSecret = this._pgBouncerServer.pgbouncerSecret;
-
       this.connectionTarget = this._pgBouncerServer.instance;
-
       this.securityGroup = this._pgBouncerServer.securityGroup;
     } else {
       this.connectionTarget = this.db;
