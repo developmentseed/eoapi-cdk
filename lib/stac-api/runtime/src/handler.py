153f82bb8b58935e51f99d442f88ca8bc9f76ff7
hrodmn <henry.rodman@gmail.com>
2025-03-03 14:38:54 -0600

    work out some changes to stac-api and titiler-pgstac handlers

--- lib/stac-api/runtime/src/handler.py
+++ lib/stac-api/runtime/src/handler.py
@@ -7,6 +7,22 @@ import os
 
 from mangum import Mangum
 
+from .config import get_secret_dict
+
+pgstac_secret_arn = os.environ["PGSTAC_SECRET_ARN"]
+
+secret = get_secret_dict(pgstac_secret_arn)
+os.environ.update(
+    {
+        "postgres_host_reader": secret["host"],
+        "postgres_host_writer": secret["host"],
+        "postgres_dbname": secret["dbname"],
+        "postgres_user": secret["username"],
+        "postgres_pass": secret["password"],
+        "postgres_port": str(secret["port"]),
+    }
+)
+
 from .app import app
 
 handler = Mangum(app, lifespan="off")
