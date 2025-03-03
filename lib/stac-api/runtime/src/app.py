9805c22235c06ce105d684eb86fc2ca7746fbfbd
hrodmn <henry.rodman@gmail.com>
2025-03-03 14:38:54 -0600

    work out some changes to stac-api and titiler-pgstac handlers

--- lib/stac-api/runtime/src/app.py
+++ lib/stac-api/runtime/src/app.py
@@ -2,40 +2,57 @@
 FastAPI application using PGStac.
 """
 
+from brotli_asgi import BrotliMiddleware
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 from stac_fastapi.api.app import StacApi
+from stac_fastapi.api.middleware import ProxyHeaderMiddleware
+from stac_fastapi.pgstac.app import (
+    application_extensions,
+    collections_get_request_model,
+    get_request_model,
+    items_get_request_model,
+    post_request_model,
+)
 from stac_fastapi.pgstac.core import CoreCrudClient
 from stac_fastapi.pgstac.db import close_db_connection, connect_to_db
-from starlette_cramjam.middleware import CompressionMiddleware
+from starlette.middleware import Middleware
 
 from .config import ApiSettings
-from .config import extensions as PgStacExtensions
-from .config import get_request_model as GETModel
-from .config import post_request_model as POSTModel
 
-api_settings = ApiSettings()
+settings = ApiSettings()
 
 api = StacApi(
-    title=api_settings.name,
-    api_version=api_settings.version,
-    description=api_settings.description or api_settings.name,
-    settings=api_settings.load_postgres_settings(),
-    extensions=PgStacExtensions,
-    client=CoreCrudClient(post_request_model=POSTModel),
-    search_get_request_model=GETModel,
-    search_post_request_model=POSTModel,
+    app=FastAPI(
+        openapi_url=settings.openapi_url,
+        docs_url=settings.docs_url,
+        redoc_url=None,
+        root_path=settings.root_path,
+        title=settings.stac_fastapi_title,
+        version=settings.stac_fastapi_version,
+        description=settings.stac_fastapi_description,
+    ),
+    settings=settings,
+    extensions=application_extensions,
+    client=CoreCrudClient(pgstac_search_model=post_request_model),
     response_class=ORJSONResponse,
-    middlewares=[CompressionMiddleware],
+    items_get_request_model=items_get_request_model,
+    search_get_request_model=get_request_model,
+    search_post_request_model=post_request_model,
+    collections_get_request_model=collections_get_request_model,
+    middlewares=[
+        Middleware(BrotliMiddleware),
+        Middleware(ProxyHeaderMiddleware),
+    ],
 )
-
 app = api.app
 
 # Set all CORS enabled origins
-if api_settings.cors_origins:
+if settings.cors_origins:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=api_settings.cors_origins,
+        allow_origins=settings.cors_origins,
         allow_credentials=True,
         allow_methods=["GET", "POST", "OPTIONS"],
         allow_headers=["*"],
--- lib/titiler-pgstac-api/runtime/src/handler.py
+++ lib/titiler-pgstac-api/runtime/src/handler.py
@@ -5,8 +5,7 @@ Handler for AWS Lambda.
 import asyncio
 import os
 
-from mangum import Mangum
-from utils import get_secret_dict
+from .utils import get_secret_dict
 
 pgstac_secret_arn = os.environ["PGSTAC_SECRET_ARN"]
 
@@ -21,14 +20,31 @@ os.environ.update(
     }
 )
 
+from mangum import Mangum
 from titiler.pgstac.db import connect_to_db  # noqa: E402
 from titiler.pgstac.main import app  # noqa: E402
+from titiler.pgstac.settings import PostgresSettings
+from utils import get_secret_dict
 
 
 @app.on_event("startup")
 async def startup_event() -> None:
     """Connect to database on startup."""
-    await connect_to_db(app)
+    pgstac_secret_arn = os.getenv("PGSTAC_SECRET_ARN")
+    if not pgstac_secret_arn:
+        raise ValueError("PGSTAC_SECRET_ARN is not set!")
+
+    secret = get_secret_dict(pgstac_secret_arn)
+    await connect_to_db(
+        app,
+        settings=PostgresSettings(
+            postgres_user=secret["username"],
+            postgres_pass=secret["password"],
+            postgres_host=secret["host"],
+            postgres_port=secret["port"],
+            postgres_dbname=secret["dbname"],
+        ),
+    )
 
 
 handler = Mangum(app, lifespan="off")
