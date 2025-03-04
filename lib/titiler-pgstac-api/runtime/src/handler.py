70d9f5367e1b67b1d8490b34eeb8b564fe45eb85
hrodmn <henry.rodman@gmail.com>
2025-03-03 20:32:01 -0600

    clean up titiler-pgstac handler

--- lib/titiler-pgstac-api/runtime/src/handler.py
+++ lib/titiler-pgstac-api/runtime/src/handler.py
@@ -4,13 +4,22 @@ Handler for AWS Lambda.
 
 import asyncio
 import os
+from functools import lru_cache
 
 from mangum import Mangum
 from utils import get_secret_dict
 
-pgstac_secret_arn = os.environ["PGSTAC_SECRET_ARN"]
 
-secret = get_secret_dict(pgstac_secret_arn)
+@lru_cache(maxsize=1)
+def get_db_credentials():
+    """Cache the credentials to avoid repeated Secrets Manager calls."""
+    pgstac_secret_arn = os.environ.get("PGSTAC_SECRET_ARN")
+    if not pgstac_secret_arn:
+        raise ValueError("PGSTAC_SECRET_ARN is not set!")
+    return get_secret_dict(pgstac_secret_arn)
+
+
+secret = get_db_credentials()
 os.environ.update(
     {
         "postgres_host": secret["host"],
@@ -20,29 +29,15 @@ os.environ.update(
         "postgres_port": str(secret["port"]),
     }
 )
+
 from titiler.pgstac.db import connect_to_db  # noqa: E402
 from titiler.pgstac.main import app  # noqa: E402
-from titiler.pgstac.settings import PostgresSettings
 
 
 @app.on_event("startup")
 async def startup_event() -> None:
     """Connect to database on startup."""
-    pgstac_secret_arn = os.getenv("PGSTAC_SECRET_ARN")
-    if not pgstac_secret_arn:
-        raise ValueError("PGSTAC_SECRET_ARN is not set!")
-
-    secret = get_secret_dict(pgstac_secret_arn)
-    await connect_to_db(
-        app,
-        settings=PostgresSettings(
-            postgres_user=secret["username"],
-            postgres_pass=secret["password"],
-            postgres_host=secret["host"],
-            postgres_port=secret["port"],
-            postgres_dbname=secret["dbname"],
-        ),
-    )
+    await connect_to_db(app)
 
 
 handler = Mangum(app, lifespan="off")
