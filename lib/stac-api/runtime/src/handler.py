671f44248c0b86d34fd46e062f43787d17fe8a3b
hrodmn <henry.rodman@gmail.com>
2025-03-06 13:28:16 -0600

    upgrade to stac_fastapi.pgstac 5.0 to get new PostgresSettings

--- lib/stac-api/runtime/src/handler.py
+++ lib/stac-api/runtime/src/handler.py
@@ -6,30 +6,27 @@ import asyncio
 import os
 
 from mangum import Mangum
+from stac_fastapi.pgstac.app import app
+from stac_fastapi.pgstac.config import PostgresSettings
+from stac_fastapi.pgstac.db import close_db_connection, connect_to_db
 from utils import get_secret_dict
 
 secret = get_secret_dict(secret_arn_env_var="PGSTAC_SECRET_ARN")
-
-os.environ.update(
-    {
-        "postgres_host_reader": secret["host"],
-        "postgres_host_writer": secret["host"],
-        "postgres_dbname": secret["dbname"],
-        "postgres_user": secret["username"],
-        "postgres_pass": secret["password"],
-        "postgres_port": str(secret["port"]),
-    }
+postgres_settings = PostgresSettings(
+    postgres_host_reader=secret["host"],
+    postgres_host_writer=secret["host"],
+    postgres_dbname=secret["dbname"],
+    postgres_user=secret["username"],
+    postgres_pass=secret["password"],
+    postgres_port=int(secret["port"]),
 )
-
-from stac_fastapi.pgstac.app import app
-from stac_fastapi.pgstac.db import close_db_connection, connect_to_db
 
 
 @app.on_event("startup")
 async def startup_event():
     """Connect to database on startup."""
     print("Setting up DB connection...")
-    await connect_to_db(app)
+    await connect_to_db(app, postgres_settings=postgres_settings)
     print("DB connection setup.")
 
 
