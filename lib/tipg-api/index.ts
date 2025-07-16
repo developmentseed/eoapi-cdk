547e3f966f91b27cb673869dd81d8224d152dc2f
Anthony Lukach <anthonylukach@gmail.com>
2025-07-16 15:26:33 -0700

    refactor(tipg): create separate constructs for runtime and API

--- lib/tipg-api/index.ts
+++ lib/tipg-api/index.ts
@@ -14,118 +14,173 @@ import { Construct } from "constructs";
 import { CustomLambdaFunctionProps } from "../utils";
 import * as path from 'path';
 
-  export class TiPgApiLambda extends Construct {
-    readonly url: string;
-    public tiPgLambdaFunction: lambda.Function;
+export class TiPgApiLambdaRuntime extends Construct {
+  public readonly tiPgLambdaFunction: lambda.Function;
 
-    constructor(scope: Construct, id: string, props: TiPgApiLambdaProps) {
-      super(scope, id);
+  constructor(scope: Construct, id: string, props: TiPgApiLambdaRuntimeProps) {
+    super(scope, id);
 
-      this.tiPgLambdaFunction = new lambda.Function(this, "lambda", {
-        // defaults
-        runtime: lambda.Runtime.PYTHON_3_11,
-        handler: "handler.handler",
-        memorySize: 1024,
-        logRetention: logs.RetentionDays.ONE_WEEK,
-        timeout: Duration.seconds(30),
-        code: lambda.Code.fromDockerBuild(path.join(__dirname, '..'), {
-          file: "tipg-api/runtime/Dockerfile",
-          buildArgs: { PYTHON_VERSION: '3.11' },
-        }),
-        vpc: props.vpc,
-        vpcSubnets: props.subnetSelection,
-        allowPublicSubnet: true,
-        environment: {
-          PGSTAC_SECRET_ARN: props.dbSecret.secretArn,
-          DB_MIN_CONN_SIZE: "1",
-          DB_MAX_CONN_SIZE: "1",
-          ...props.apiEnv,
-        },
-        // overwrites defaults with user-provided configurable properties
-        ...props.lambdaFunctionOptions
-      });
+    this.tiPgLambdaFunction = new lambda.Function(this, "lambda", {
+      // defaults
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: "handler.handler",
+      memorySize: 1024,
+      logRetention: logs.RetentionDays.ONE_WEEK,
+      timeout: Duration.seconds(30),
+      code: lambda.Code.fromDockerBuild(path.join(__dirname, ".."), {
+        file: "tipg-api/runtime/Dockerfile",
+        buildArgs: { PYTHON_VERSION: "3.11" },
+      }),
+      vpc: props.vpc,
+      vpcSubnets: props.subnetSelection,
+      allowPublicSubnet: true,
+      environment: {
+        PGSTAC_SECRET_ARN: props.dbSecret.secretArn,
+        DB_MIN_CONN_SIZE: "1",
+        DB_MAX_CONN_SIZE: "1",
+        ...props.apiEnv,
+      },
+      // overwrites defaults with user-provided configurable properties
+      ...props.lambdaFunctionOptions,
+    });
 
-      props.dbSecret.grantRead(this.tiPgLambdaFunction);
+    props.dbSecret.grantRead(this.tiPgLambdaFunction);
 
-      if (props.vpc){
-        this.tiPgLambdaFunction.connections.allowTo(props.db, ec2.Port.tcp(5432), "allow connections from tipg");
-      }
-
-      const tipgApi = new apigatewayv2.HttpApi(
-        this,
-        `${Stack.of(this).stackName}-tipg-api`,
-        {
-          defaultDomainMapping: props.tipgApiDomainName
-            ? {
-                domainName: props.tipgApiDomainName,
-              }
-            : undefined,
-          defaultIntegration:
-            new apigatewayv2_integrations.HttpLambdaIntegration(
-              "integration",
-              this.tiPgLambdaFunction,
-              props.tipgApiDomainName
-                ? {
-                    parameterMapping:
-                      new apigatewayv2.ParameterMapping().overwriteHeader(
-                        "host",
-                        apigatewayv2.MappingValue.custom(
-                          props.tipgApiDomainName.name
-                        )
-                      ),
-                  }
-                : undefined
-            ),
-        }
+    if (props.vpc) {
+      this.tiPgLambdaFunction.connections.allowTo(
+        props.db,
+        ec2.Port.tcp(5432),
+        "allow connections from tipg"
       );
-
-      this.url = tipgApi.url!;
-
-      new CfnOutput(this, "tipg-api-output", {
-        exportName: `${Stack.of(this).stackName}-tip-url`,
-        value: this.url,
-      });
     }
   }
+}
 
-  export interface TiPgApiLambdaProps {
-    /**
-     * VPC into which the lambda should be deployed.
-     */
-    readonly vpc?: ec2.IVpc;
+export interface TiPgApiLambdaRuntimeProps {
+  /**
+   * VPC into which the lambda should be deployed.
+   */
+  readonly vpc?: ec2.IVpc;
 
-    /**
-     * RDS Instance with installed pgSTAC or pgbouncer server.
-     */
-    readonly db: rds.IDatabaseInstance | ec2.IInstance;
+  /**
+   * RDS Instance with installed pgSTAC or pgbouncer server.
+   */
+  readonly db: rds.IDatabaseInstance | ec2.IInstance;
 
-    /**
-     * Subnet into which the lambda should be deployed.
-     */
-    readonly subnetSelection?: ec2.SubnetSelection;
+  /**
+   * Subnet into which the lambda should be deployed.
+   */
+  readonly subnetSelection?: ec2.SubnetSelection;
 
-    /**
-     * Secret containing connection information for pgSTAC database.
-     */
-    readonly dbSecret: secretsmanager.ISecret;
+  /**
+   * Secret containing connection information for pgSTAC database.
+   */
+  readonly dbSecret: secretsmanager.ISecret;
 
-    /**
-     * Customized environment variables to send to titiler-pgstac runtime.
-     */
-    readonly apiEnv?: Record<string, string>;
+  /**
+   * Customized environment variables to send to titiler-pgstac runtime.
+   */
+  readonly apiEnv?: Record<string, string>;
 
-    /**
-     * Custom Domain Name for tipg API. If defined, will create the
-     * domain name and integrate it with the tipg API.
-     *
-     * @default - undefined
-     */
-    readonly tipgApiDomainName?: apigatewayv2.IDomainName;
+  /**
+   * Can be used to override the default lambda function properties.
+   *
+   * @default - defined in the construct.
+   */
+  readonly lambdaFunctionOptions?: CustomLambdaFunctionProps;
+}
 
-    /**
-     * Can be used to override the default lambda function properties.
-     *
-     * @default - defined in the construct.
-     */
-    readonly lambdaFunctionOptions?: CustomLambdaFunctionProps;
+export class TiPgApiLambdaApiGateway extends Construct {
+  readonly url: string;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props: TiPgApiLambdaApiGatewayProps
+  ) {
+    super(scope, id);
+
+    const tipgApi = new apigatewayv2.HttpApi(
+      this,
+      `${Stack.of(this).stackName}-tipg-api`,
+      {
+        defaultDomainMapping: props.tipgApiDomainName
+          ? {
+              domainName: props.tipgApiDomainName,
+            }
+          : undefined,
+        defaultIntegration: new apigatewayv2_integrations.HttpLambdaIntegration(
+          "integration",
+          props.lambdaFunction,
+          props.tipgApiDomainName
+            ? {
+                parameterMapping:
+                  new apigatewayv2.ParameterMapping().overwriteHeader(
+                    "host",
+                    apigatewayv2.MappingValue.custom(
+                      props.tipgApiDomainName.name
+                    )
+                  ),
+              }
+            : undefined
+        ),
+      }
+    );
+
+    this.url = tipgApi.url!;
+
+    new CfnOutput(this, "tipg-api-output", {
+      exportName: `${Stack.of(this).stackName}-tip-url`,
+      value: this.url,
+    });
   }
+}
+
+export interface TiPgApiLambdaApiGatewayProps {
+  /**
+   * Lambda function to integrate with the API Gateway.
+   */
+  readonly lambdaFunction: lambda.Function;
+
+  /**
+   * Custom Domain Name for tipg API. If defined, will create the
+   * domain name and integrate it with the tipg API.
+   *
+   * @default - undefined
+   */
+  readonly tipgApiDomainName?: apigatewayv2.IDomainName;
+}
+
+export class TiPgApiLambda extends Construct {
+  readonly url: string;
+  public tiPgLambdaFunction: lambda.Function;
+
+  constructor(scope: Construct, id: string, props: TiPgApiLambdaProps) {
+    super(scope, id);
+
+    const runtime = new TiPgApiLambdaRuntime(this, "runtime", {
+      vpc: props.vpc,
+      subnetSelection: props.subnetSelection,
+      db: props.db,
+      dbSecret: props.dbSecret,
+      apiEnv: props.apiEnv,
+      lambdaFunctionOptions: props.lambdaFunctionOptions,
+    });
+    this.tiPgLambdaFunction = runtime.tiPgLambdaFunction;
+
+    const api = new TiPgApiLambdaApiGateway(this, "api", {
+      lambdaFunction: runtime.tiPgLambdaFunction,
+      tipgApiDomainName: props.tipgApiDomainName,
+    });
+
+    this.url = api.url;
+  }
+}
+
+export interface TiPgApiLambdaProps
+  extends TiPgApiLambdaRuntimeProps,
+    Omit<TiPgApiLambdaApiGatewayProps, "lambdaFunction"> {
+  // This interface combines both runtime and API gateway props
+  // The lambdaFunction property from TiPgApiLambdaApiGatewayProps is excluded
+  // as it will be provided by the runtime construct
+}
