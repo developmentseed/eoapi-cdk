7312e3119a087828e87b46a7af2e1067c51b251a
Anthony Lukach <anthonylukach@gmail.com>
2025-08-05 11:39:42 -0700

    refactor: simplify stac-auth-proxy handler

--- lib/stac-auth-proxy/runtime/src/handler.py
+++ lib/stac-auth-proxy/runtime/src/handler.py
@@ -2,16 +2,8 @@
 Handler for AWS Lambda.
 """
 
-import asyncio
-import os
-
 from mangum import Mangum
 from stac_auth_proxy import create_app
 
 app = create_app()
 handler = Mangum(app, lifespan="off")
-
-
-if "AWS_EXECUTION_ENV" in os.environ:
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(app.router.startup())
